5a7b863f57a6e6283ced452aec3455bf47941cd8
MohammadFahad1 <fahad4bangladesh@gmail.com>
2022-11-10 10:14:26 +0600

    JWT Auth Added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -44,6 +44,22 @@ const Login = () => {
         signIn(email, password)
             .then(result => {
                 toast.success('Successfully Logged In!');
+                const user = result.user;
+
+                const currentUser = {
+                    email: user.email
+                }
+
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                }).then(res => res.json())
+                    .then(data => {
+                        localStorage.setItem('lawFarm-token', data.token);
+                    })
                 navigate(from)
                 form.reset();
             })
--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -11,9 +11,14 @@ const MyReviews = () => {
     const { user } = useContext(AuthContext);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/myreviews/${user.uid}`)
+        fetch(`http://localhost:5000/myreviews/${user.uid}?email=${user.email}`, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('lawFarm-token')}`
+            }
+        })
             .then(res => res.json())
             .then(review => {
+                console.log(review);
                 setReviews(review);
                 setLoading(false)
             })
@@ -49,13 +54,11 @@ const MyReviews = () => {
             <Toaster></Toaster>
             {
                 reviews?.length > 0 ?
-                    reviews?.map(
-                        review => <ReviewCard key={review._id} review={review} handleDelete={handleDelete}></ReviewCard>
-                    )
+                    reviews?.map(review => <ReviewCard key={review._id} review={review} handleDelete={handleDelete}></ReviewCard>)
                     :
-                    <div id="alert-border-2" class="flex p-4 text-xl mb-4 bg-red-100 border-t-4 border-red-500 dark:bg-red-200" role="alert">
-                        <svg class="flex-shrink-0 w-5 h-5 text-red-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-                        <div class="ml-3 font-medium text-red-700">
+                    <div id="alert-border-2" className="flex p-4 text-xl mb-4 bg-red-100 border-t-4 border-red-500 dark:bg-red-200" role="alert">
+                        <svg className="flex-shrink-0 w-5 h-5 text-red-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+                        <div className="ml-3 font-medium text-red-700">
                             No reviews have been Found!
                         </div>
                     </div>
